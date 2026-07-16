ebd19d81374deda94627cfb49d6616e20bfa3b4e
Eliane-Borda <eliane.borda@pucp.edu.pe>
2026-07-15 20:43:41 -0500

    Update config.py

--- cliente_clinico/config.py
+++ cliente_clinico/config.py
@@ -1,7 +1,7 @@
 # config.py
 
 # Cambiar únicamente esta variable cuando exista la instancia EC2
-SERVER_URL = "http://localhost:5000"
+SERVER_URL = "https://healthymed-sedecentral.duckdns.org"
 
 # Tiempo máximo de espera para peticiones HTTP (segundos)
 REQUEST_TIMEOUT = 10
